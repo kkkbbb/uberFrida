be3f4773e454c5f6507ad115095e3e9f4210e6a2
wwb <wwuwenbiao@gmail.com>
2025-06-13 14:59:23 +0800

    uber

--- examples/core/uber/src/main.rs
+++ examples/core/uber/src/main.rs
@@ -1,13 +1,17 @@
 #![cfg(all(target_os = "android", target_arch = "aarch64"))]
+#![recursion_limit = "256"]
 
 use frida::{DeviceManager, Frida, Message, ScriptHandler, ScriptOption, ScriptRuntime, SpawnOptions};
 use std::sync::{LazyLock, Mutex};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use base64::{engine::general_purpose, Engine};
 use serde::Serialize;
 use serde_json::{json, Value};
 use uuid::Uuid;
+use rand::{random, Rng};
+use hex;
+use reqwest::Url;
 
 static FRIDA: LazyLock<Frida> = LazyLock::new(|| unsafe { Frida::obtain() });
 static COLLECT_DATA: LazyLock<Mutex<CollectData>> = LazyLock::new(||{Mutex::new(CollectData::default())});
@@ -18,131 +22,9 @@ struct CollectData {
     code_verifier: Option<String>,
 }
 
-const JSON_TEMPLATE: &str = r#"
-{
-    "register_url":"",
-    "pack1":{
-        "url":"https://cn-geo1.uber.com/rt/silk-screen/submit-form",
-        "headers":{
-          "User-Agent": "Cronet/119.0.6045.31@c76b9b6a",
-          "Accept": "*/*",
-          "Accept-Encoding": "gzip, deflate",
-          "Content-Type": "application/json",
-          "x-uber-device-location-accuracy": "5.0",
-          "x-uber-device-mobile-iso2": "CN",
-          "x-uber-device-location-longitude": "100.5649717",
-          "x-uber-device": "android",
-          "x-uber-device-language": "zh_CN",
-          "x-uber-device-location-altitude": "0.0",
-          "x-uber-device-os": "12",
-          "x-uber-device-sdk": "31",
-          "x-uber-client-version": "4.575.10001",
-          "x-uber-session-swap-mobile": "FALSE",
-          "x-uber-device-manufacturer": "Google",
-          "x-uber-device-width-pixel": "1440",
-          "x-uber-device-location-speed": "0.0",
-          "x-uber-device-model": "",
-          "x-uber-device-height-pixel": "2872"
-        },
-        "body":{
-          "formContainerAnswer": {
-            "inAuthSessionID": "xxx",
-            "formAnswer": {
-              "flowType": "SIGN_IN",
-              "screenAnswers": [
-                {
-                  "screenType": "SESSION_VERIFICATION",
-                  "fieldAnswers": [
-                    {
-                      "fieldType": "SESSION_VERIFICATION_CODE",
-                      "sessionVerificationCode": "xxx"
-                    },
-                    {
-                      "fieldType": "CODE_VERIFIER",
-                      "codeVerifier": "xxxxxxx"
-                    }
-                  ],
-                  "eventType": "TypeVerifySession"
-                }
-              ],
-              "deviceData": "xxxx",
-              "firstPartyClientID": "zozycDbnl17oSjKXdw_x_QuNvq5wfRHq",
-              "standardFlow": true
-            }
-          }
-        }
-    },
-    "pack2": {
-        "url": "https://cn-geo1.uber.com/rt/users/arch-signin-token",
-        "headers": {
-          "authorization": "xxxx",
-          "uberctx-is-admin": "null",
-          "x-uber-client-id": "com.ubercab",
-          "x-uber-app-lifecycle-state": "foreground",
-          "x-uber-device-timezone": "Asia/Shanghai",
-          "x-uber-device-location-course": "90.0",
-          "x-uber-client-name": "client",
-          "x-uber-device-time-24-format-enabled": "0",
-          "x-uber-device-scale-factor": "3.5",
-          "x-uber-session-swap-bb8": "FALSE"
-        },
-        "body": {
-          "request": {
-            "stateToken": "xxxxx",
-            "nextURL": "https://xlb.uber.com",
-            "allCookies": true
-          }
-        }
-    }
-}
-"#;
 
-const DEVICE_DATA_TEMPLATE: &str = r#"
-{
-    "androidId": "959f93521cb5f5d4",
-    "batteryLevel": 0.65,
-    "batteryStatus": "unplugged",
-    "carrier": "T-Mobile",
-    "carrierMcc": "310",
-    "carrierMnc": "260",
-    "course": 90,
-    "cpuAbi": ", arm64-v8a",
-    "deviceAltitude": 0,
-    "deviceIds": {
-        "androidId": "959f93521cb5f5d4",
-        "appDeviceId": "b7057f0a-b5ab-3239-bde2-d5d9b4f6622a",
-        "drmId": "43DF21DB79C145E05E6F9B1BDEB07A1E191DAD978874B23EB85A0DED3A9CD1A9",
-        "googleAdvertisingId": "f936bfb8-e03c-46be-adf0-5b7b7058699d",
-        "googleAppSetId": "4a88aa18-2cd2-44a8-5dfe-bd0fc00ae6d2",
-        "installationUuid": "4021a604-a347-4731-a07a-c62e94d7814c",
-        "perfId": "842714B5-5E47-D904-E4B6-2F6A63511ECE",
-        "udid": "cc83b5e5-0b9e-3a27-8e44-459b38bc5b67"
-    },
-    "deviceLatitude": 22.378755,
-    "deviceLongitude": 100.5649717,
-    "deviceModel": "sdk_gphone64_arm64",
-    "deviceOsName": "Android",
-    "deviceOsVersion": "12",
-    "emulator": true,
-    "epoch": {
-        "value": 1749030937571
-    },
-    "horizontalAccuracy": 5,
-    "ipAddress": "10.0.2.16",
-    "libCount": 0,
-    "locationServiceEnabled": true,
-    "mockGpsOn": false,
-    "rooted": false,
-    "sourceApp": "rider",
-    "specVersion": "2.0",
-    "speed": 0,
-    "systemTimeZone": "Asia/Shanghai",
-    "version": "4.575.10001",
-    "versionChecksum": "f8aa5a20c5a5d0f1802c88508a5a3c2c",
-    "verticalAccuracy": 0,
-    "wifiConnected": true
-}
-"#;
+
+
 
 impl CollectData {
     fn is_complete(&self) -> bool {
@@ -175,9 +57,10 @@ fn main() {
                 return this.loadUrl.apply(this,arguments);
             }
             enumClass("com.uber.identity.api.uauth.internal.helper.f").a.overload("java.lang.String").implementation = function() {
-                let verifier = this.a.apply(this,arguments);
-                console.log("verifier "+arguments[0]);
-                return verifier;
+                let challenge = this.a.apply(this,arguments);
+                console.log("chanllenge "+challenge);
+                console.log("verifier "+ arguments[0]);
+                return challenge;
             }
         })
     "#;
@@ -224,11 +107,13 @@ impl ScriptHandler for Handler {
            Message::Log(log) => {
                let raw_data = log.payload.split(' ').collect::<Vec<&str>>();
                if raw_data[0].to_string() == "url" {
-                   println!("[*] collect url");
+                   println!("[*] collect url {}",log.payload);
                    COLLECT_DATA.lock().unwrap().register_url = Some(raw_data[1].to_string());
                }else if raw_data[0].to_string() == "verifier" {
-                   println!("[*] collect verifier");
+                   println!("[*] collect verifier {}",log.payload);
                    COLLECT_DATA.lock().unwrap().code_verifier = Some(raw_data[1].to_string());
+               }else {
+                   println!("[*] Unknown log {}", log.payload);
                }
 
            },
@@ -240,14 +125,28 @@ impl ScriptHandler for Handler {
 
         if COLLECT_DATA.lock().unwrap().is_complete() {
             let reg_url = COLLECT_DATA.lock().unwrap().register_url.clone().unwrap();
-            let mut pack_data:Value = serde_json::from_str(JSON_TEMPLATE).unwrap();
-            let mut device_data:Value = serde_json::from_str(DEVICE_DATA_TEMPLATE).unwrap();
+
+            let mut first_party_client_id = String::new();
+            let mut app_device_id = String::new();
+            let mut uber_cold_launch_id = String::new();
+            for (key,value) in Url::parse(&reg_url).unwrap().query_pairs() {
+                if key == "firstPartyClientID" {
+                    first_party_client_id = value.to_string();
+                }else if key == "app_url" {
+                    for (key1,value1) in Url::parse(&reg_url).unwrap().query_pairs() {
+                        if key1 == "x-uber-app-device-id" {
+                            app_device_id = value1.to_string();
+                        }else if key1 == "x-uber-cold-launch-id" {
+                            uber_cold_launch_id = value1.to_string();
+                        }
+                    }
+                }
+            }
+
+            let mut pack_data:Value = gen_request(&*first_party_client_id, &*app_device_id, &*uber_cold_launch_id);
             pack_data["register_url"] = json!(reg_url);
             pack_data["pack1"]["body"]["formContainerAnswer"]["formAnswer"]["screenAnswers"][0]["fieldAnswers"][1]["codeVerifier"] = json!(COLLECT_DATA.lock().unwrap().code_verifier.clone().unwrap().to_string());
-            
-            device_data["deviceIds"]["installationUuid"] = json!(Uuid::new_v4().to_string());
-            
-            pack_data["formContainerAnswer"]["formAnswer"]["deviceData"] = json!(device_data.to_string());
+
             println!("JSON string: {}", pack_data.to_string());
             let base64_encoded = general_purpose::STANDARD.encode(pack_data.to_string());
             println!("Base64 encoded: {}", base64_encoded);
@@ -258,4 +157,199 @@ impl ScriptHandler for Handler {
             println!("[*] Response: {:?}", response);
         }
     }
+}
+
+pub fn gen_request(first_party_client_id:&str, app_device_id:&str, cold_launch_id:&str) -> Value {
+    let mut rng = rand::rng();
+    let uber_request_uuid = Uuid::new_v4().to_string();
+    let drm_bytes: [u8; 32] = random();
+    let drm_id = hex::encode_upper(drm_bytes).to_uppercase();
+    let user_session_id = Uuid::new_v4().to_string();
+    let call_uuid = Uuid::new_v4().to_string();
+    let device_id_byte: [u8; 32] = random();
+    let uber_device_id = hex::encode(device_id_byte);
+    let network_request_uuid = Uuid::new_v4().to_string();
+    let device_epoch = format!("{}{}",SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis() as u64,rng.random_range(1000..9999));
+    // let uberctx_cold_launch_id = Uuid::new_v4().to_string();
+    let uber_client_session = Uuid::new_v4().to_string();
+    let uber_id = Uuid::new_v4().to_string();
+    let state_token = Uuid::new_v4().to_string();
+
+    let android_id_bytee:[u8;8] = rng.random();
+    let android_id = hex::encode(android_id_bytee);
+    let google_ad_id = Uuid::new_v4().to_string();
+    let google_ad_set_id = Uuid::new_v4().to_string();
+    let install_uuid = Uuid::new_v4().to_string();
+    let pref_id = Uuid::new_v4().to_string();
+    let uuid_id = Uuid::new_v3(&Uuid::NAMESPACE_DNS, pref_id.as_bytes()).to_string();
+
+
+    let device_data_template: Value = json!({
+        "androidId": android_id,
+        "batteryLevel": 0.65,
+        "batteryStatus": "unplugged",
+        "carrier": "T-Mobile",
+        "carrierMcc": "310",
+        "carrierMnc": "260",
+        "course": 90,
+        "cpuAbi": ", arm64-v8a",
+        "deviceAltitude": 0,
+        "deviceIds": {
+            "androidId": android_id,
+            "appDeviceId": app_device_id,
+            "drmId":drm_id,
+            "googleAdvertisingId": google_ad_id,
+            "googleAppSetId": google_ad_set_id,
+            "installationUuid": install_uuid,
+            "perfId": pref_id,
+            "udid": uuid_id
+        },
+        "deviceLatitude": 22.378755,
+        "deviceLongitude": 100.5649717,
+        "deviceModel": "sdk_gphone64_arm64",
+        "deviceOsName": "Android",
+        "deviceOsVersion": "12",
+        "emulator": true,
+        "epoch": {
+            "value": device_epoch,
+        },
+        "horizontalAccuracy": 5,
+        "ipAddress": "10.0.2.16",
+        "libCount": 0,
+        "locationServiceEnabled": true,
+        "mockGpsOn": false,
+        "rooted": false,
+        "sourceApp": "rider",
+        "specVersion": "2.0",
+        "speed": 0,
+        "systemTimeZone": "Asia/Shanghai",
+        "version": "4.575.10001",
+        "versionChecksum": "f8aa5a20c5a5d0f1802c88508a5a3c2c",
+        "verticalAccuracy": 0,
+        "wifiConnected": true});
+
+        let json_template: Value = json!({
+        "register_url":"",
+        "pack1":{
+            "url":"https://cn-geo1.uber.com/rt/silk-screen/submit-form",
+            "headers":{
+              "User-Agent": "Cronet/119.0.6045.31@c76b9b6a",
+              "Content-Type": "application/json",
+              "x-uber-device-mobile-iso2": "CN",
+              "x-uber-drm-id": drm_id,
+              "x-uber-device": "android",
+              "x-uber-device-language": "zh_CN",
+              "x-uber-device-os": "12",
+              "x-uber-device-sdk": "31",
+              "x-uber-request-uuid": uber_request_uuid,
+              "x-uber-client-user-session-id": user_session_id,
+              "x-uber-client-version": "4.575.10001",
+              "x-uber-session-swap-mobile": "TRUE",
+              "x-uber-device-manufacturer": "redroid",
+              "x-uber-device-width-pixel": "720",
+              "x-uber-call-uuid": call_uuid,
+              "x-uber-device-id": uber_device_id,
+              "x-uber-device-model": "redroid12_arm64",
+              "x-uber-device-height-pixel": "1184",
+              "uberctx-mobile-initiated": "true",
+              "x-uber-app-variant": "",
+              "uberctx-client-network-request-uuid": network_request_uuid,
+              "x-uber-session-enabled": "TRUE",
+              "x-uber-device-epoch": device_epoch,
+              "x-uber-session": "eyJ1c2VyX3Nlc3Npb24iOiJaWGxLZWxwWVRucGhWemwxV0RKc2EwbHFjRGRKYmxZeFlWZFJhVTl1YzJsa2JVWnpaRmRWYVU5cFNYZE9SMGt4V1hwTk5FOURNSGROVjFKcVRGUlNiRTE2UVhSUFZFcHNXWGt4YkZsNldYbE9WRWswVDFSck5FMUVTV2xtV0RCelNXMVdOR05IYkhsYVdFNW1XVmhSYVU5dWMybGtiVVp6WkZkVmFVOXFSVE5PUkd0M1RVUnJlazU2WXpCTmFrWTVURU5LYW1OdFZtaGtSMVpyV0RKR01FbHFjRGRKYmxwb1lraFdiRWxxYjNoT2VsRTFUVVJCTTA1VVl6Tk9SRWw0Wmxnd1BRPT0iLCJjb29raWVfZXhwaXJlc19hdCI6eyJ2YWx1ZSI6MTc0OTAwNzU5NzMxN30sImNvb2tpZV9jcmVhdGVkX2F0Ijp7InZhbHVlIjoxNzQ5MDA3NTg3MzE3fSwiYWN0aW9uIjoyfQ",
+              "uberctx-cold-launch-id": cold_launch_id,
+              "uberctx-is-admin": "null",
+              "x-uber-client-id": "com.ubercab",
+              "x-uber-app-lifecycle-state": "foreground",
+              "x-uber-device-timezone": "Asia/Shanghai",
+              "x-uber-client-name": "client",
+              "x-uber-client-session": uber_client_session,
+              "x-uber-device-time-24-format-enabled": "0",
+              "x-uber-device-scale-factor": "2.0",
+              "x-uber-app-device-id": app_device_id,
+              "x-uber-session-swap-bb8": "FALSE"
+            },
+            "body":{
+              "formContainerAnswer": {
+                "inAuthSessionID": "xxx",
+                "formAnswer": {
+                  "flowType": "SIGN_IN",
+                  "screenAnswers": [
+                    {
+                      "screenType": "SESSION_VERIFICATION",
+                      "fieldAnswers": [
+                        {
+                          "fieldType": "SESSION_VERIFICATION_CODE",
+                          "sessionVerificationCode": "xxx"
+                        },
+                        {
+                          "fieldType": "CODE_VERIFIER",
+                          "codeVerifier": "xxxxxxx"
+                        }
+                      ],
+                      "eventType": "TypeVerifySession"
+                    }
+                  ],
+                  "deviceData": device_data_template.to_string(),
+                  "firstPartyClientID": first_party_client_id,
+                  "standardFlow": true
+                }
+              }
+            }
+        },
+        "pack2": {
+            "url": "https://cn-geo1.uber.com/rt/users/arch-signin-token",
+            "headers": {
+              "User-Agent": "Cronet/119.0.6045.31@c76b9b6a",
+              "Content-Type": "application/json",
+              "x-uber-device-mobile-iso2": "CN",
+              "x-uber-drm-id": drm_id,
+              "x-uber-device": "android",
+              "x-uber-device-language": "zh_CN",
+              "authorization": "Bearer xxxx",
+              "x-uber-device-os": "12",
+              "x-uber-device-sdk": "31",
+              "x-uber-request-uuid": uber_request_uuid,
+              "x-uber-client-user-session-id": user_session_id,
+              "x-uber-client-version": "4.575.10001",
+              "x-uber-session-swap-mobile": "FALSE",
+              "x-uber-device-manufacturer": "redroid",
+              "x-uber-device-width-pixel": "720",
+              "x-uber-call-uuid": call_uuid,
+              "x-uber-device-id": uber_device_id,
+              "x-uber-id": uber_id,
+              "x-uber-device-model": "redroid12_arm64",
+              "x-uber-device-height-pixel": "1184",
+              "uberctx-mobile-initiated": "true",
+              "x-uber-app-variant": "",
+              "x-uber-token": "no-token",
+              "uberctx-client-network-request-uuid": network_request_uuid,
+              "x-uber-session-enabled": "TRUE",
+              "x-uber-device-epoch": device_epoch,
+              "x-uber-session": "eyJ1c2VyX3Nlc3Npb24iOiJaWGxLZWxwWVRucGhWemwxV0RKc2EwbHFjRGRKYmxZeFlWZFJhVTl1YzJsa2JVWnpaRmRWYVU5cFNURmFSMFV6VDBScmQwMVRNV2xOUkZsM1RGUlJOVmxVVlhSUFJFbDRXV2t4YkU1cVVUUk9NbGw2V2tSTmVsa3lWV2xtV0RCelNXMVdOR05IYkhsYVdFNW1XVmhSYVU5dWMybGtiVVp6WkZkVmFVOXFSVE5PUkd0M1RXcFJNMDlVVVRCT2VrSTVURU5LYW1OdFZtaGtSMVpyV0RKR01FbHFjRGRKYmxwb1lraFdiRWxxYjNoT2VsRTFUVVJKZDAxNlRUUk5hazB5Wmxnd1BRPT0iLCJjb29raWVfZXhwaXJlc19hdCI6eyJ2YWx1ZSI6MTc0OTAyMzAwNDQ3Mn0sImNvb2tpZV9jcmVhdGVkX2F0Ijp7InZhbHVlIjoxNzQ5MDIyOTk0NDcyfSwiYWN0aW9uIjoyfQ",
+              "uberctx-cold-launch-id": cold_launch_id,
+              "uberctx-is-admin": "null",
+              "x-uber-client-id": "com.ubercab",
+              "x-uber-app-lifecycle-state": "foreground",
+              "x-uber-device-timezone": "Asia/Shanghai",
+              "x-uber-client-name": "client",
+              "x-uber-client-session": uber_client_session,
+              "x-uber-device-time-24-format-enabled": "0",
+              "x-uber-device-scale-factor": "2.0",
+              "x-uber-app-device-id": app_device_id,
+              "x-uber-session-swap-bb8": "FALSE"
+            },
+            "body": {
+              "request": {
+                "stateToken": state_token,
+                "nextURL": "https://xlb.uber.com",
+                "allCookies": true
+              }
+            }
+        }
+    });
+    json_template
 }
